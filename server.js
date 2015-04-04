ba009249a4340f54eee38e02f55de5da2a41fa92
baveltman <baveltman@gmail.com>
2015-04-03 19:19:43 -0500

    GET users/:id

--- server.js
+++ server.js
@@ -13,3 +13,42 @@ var connection = mysql.createConnection({ // Mysql Connection
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // Body parser use JSON data
+
+
+// GET /users/{facebookId}
+app.get('/quote/:facebookId', function(req, res) {
+  
+	//ensure id was properly passed
+	if(!id || req.params.facebookId < 0) {
+		res.statusCode = 404;
+		return res.send('Error 404: no user found');
+	}  
+
+	//get id from request
+	var id = req.params.facebookId;
+	
+	//initialize return object
+	var data = {
+        "error":1,
+        "user":""
+    };
+    
+    if(!!id){
+    	//query for return
+        connection.query("SELECT * FROM users WHERE facebookId=?",[Id],function(err, rows, fields){
+            if(!!err){
+            	//handle error
+                data["user"] = "Error getting user data";
+            }else{
+            	//return data successfully
+                data["error"] = 0;
+                data["user"] = rows;
+            }
+            res.json(data);
+        });
+    }else{
+    	//handle error
+        data["user"] = "Please provide userid to retrieve data";
+        res.json(data);
+    }
+});
