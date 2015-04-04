106bdca804bf3d1ada90dd446a2cbc6aadcc7618
baveltman <baveltman@gmail.com>
2015-04-04 14:05:40 -0500

    trying to fix prod port routing

--- server.js
+++ server.js
@@ -8,11 +8,19 @@ app.use(bodyParser.json()); // Body parser use JSON data
 //include routes
 require('./routes')(app);
 
+// sets port 3000 to default or unless otherwise specified in the environment
+app.set('port', process.env.PORT || 3000);
+
 
 // //have the server listen locally at port
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
 
 
+});
+
+//temporary index
+app.get('/', function(req, res){
+    res.send('welcome to dog park'); 
 });
 
 
